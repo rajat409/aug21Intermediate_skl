34d0b54894f26976919731bd7b3723a2e538766e
rajat409 <104922800+rajat409@users.noreply.github.com>
2022-05-04 22:21:27 +0530

    Update Client.java

--- src/designpatterns/factory/after/Client.java
+++ src/designpatterns/factory/after/Client.java
@@ -9,13 +9,23 @@ import designpatterns.factory.after.material.MaterialUITheme;
 
 public class Client {
     private static final String THEME = "ios";
+    //private static final String THEME = "material"; this creates for material rest all same
+    
 
     public static void main(String[] args) {
-        Theme theme = ThemeFactory.createThemeForIdentifier(THEME);
+        Theme theme = ThemeFactory.createThemeForIdentifier(THEME); // practical factory
+       
+        // wo above practical factory
+        // Theme theme;
+        // if(theme=="material")
+        //  theme=new MaterialUITheme();
+        // else
+        //  theme=new IOSUITheme();
+        
         ThemeComponentFactory themeComponentFactory = theme
-                .createThemeComponentFactory();
+                .createThemeComponentFactory(); // this is method factort
 
-        themeComponentFactory.createButton();
+        themeComponentFactory.createButton(); 
         themeComponentFactory.createMenu();
         themeComponentFactory.createDropDown();
 
